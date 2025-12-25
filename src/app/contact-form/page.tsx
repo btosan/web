6db8329c946b97b224df3919feb9408b03522be3
Best Tosan <besttosan@gmail.com>
2025-12-25 20:33:57 +0100

    contact-fporm

--- src/app/contact-form/page.tsx
+++ src/app/contact-form/page.tsx
@@ -1,13 +1,14 @@
 import GoogleEnquiryForm from '@/components/GoogleEnquiryForm'
-import React from 'react'
+import React, { Suspense } from 'react'
 
-
-function page() {
+function ContactFormPage() {
   return (
     <div className='pb-16 pt-8 px-5 md:px-0 bg-gray-800'>
+      <Suspense fallback={<div className="text-center py-10 text-gray-400">Loading form...</div>}>
         <GoogleEnquiryForm />
+      </Suspense>
     </div>
   )
 }
 
-export default page
+export default ContactFormPage
