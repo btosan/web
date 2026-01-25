cd72b88284a78bae7c6c2fa1dd9872ba426ae8f6
Best Tosan <besttosan@gmail.com>
2026-01-25 23:30:28 +0100

    text

--- src/components/PortfolioSection.tsx
+++ src/components/PortfolioSection.tsx
@@ -82,17 +82,17 @@ const ProjectCard = ({ project, index, onUserInteract }: { project: Project; ind
             href={project.liveUrl}
             target="_blank"
             rel="noopener noreferrer"
-            className="inline-flex items-center justify-center gap-2 px-3 md:px-4 py-2.5 md:py-3 text-sm text-white bg-linear-to-bl from-purple-900 via-purple-600 to-indigo-700 hover:border-0 transition-all duration-300"
+            className="inline-flex items-center justify-center gap-1 px-3 py-2.5 md:py-3 text-sm text-white bg-linear-to-bl from-purple-900 via-purple-600 to-indigo-700 hover:border-0 transition-all duration-300"
           >
             <ExternalLink size={18} />
-            View Live Site
+            View Site
           </a>
 
           <a
             href={project.detailUrl}
             target="_blank"
             rel="noopener noreferrer"
-            className="inline-flex items-center justify-center gap-2 px-3 md:px-4 py-2.5 md:py-3 text-sm lg:text-sm text-white/90 border border-gray-700 hover:border-purple-50/60 transition group/link"
+            className="inline-flex items-center justify-center gap-1 px-3 py-2.5 md:py-3 text-sm lg:text-sm text-white/90 border border-gray-700 hover:border-purple-50/60 transition group/link"
           >
             Project Details
             <ArrowRight size={18} className="transition-transform group-hover/link:translate-x-1" />
