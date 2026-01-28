1c0b523d08838f85be732e750164de338666e919
Best Tosan <besttosan@gmail.com>
2026-01-28 11:24:05 +0100

    ggg

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -84,7 +84,7 @@ export default function HeroSection() {
             lg:max-w-2xl
             xl:w-[70%]
           ">
-           We design high-performance websites and custom apps that integrate AI and intelligent automation with great UI/UX to drive measurable growth.
+           We design high-performance websites and custom apps that integrate AI and intelligent automation with great UI/UX to drive measurable growth
           </p>
 
           {/* Primary CTA Button — Now opens the modal */}
