cc8c8a6a5372867163d685f98fd936328f3ff244
Best Tosan <besttosan@gmail.com>
2026-03-25 15:22:26 +0100

    ggggggglllll

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,19 +1,14 @@
+import { Suspense } from "react";
+import SignUpForm from "@/components/forms/SignUpForm";
 
-import Link from 'next/link';
-import { ArrowRightIcon } from '@heroicons/react/24/solid';
-import { ArrowLeftIcon } from '@heroicons/react/24/solid';
-import { BsPersonAdd } from 'react-icons/bs';
-import SignUpForm from '@/components/forms/SignUpForm';
-
-const page = () => {
+export default function Page() {
   return (
-    <div className='w-full py-12 '>
-      <div className='md:px-16 lg:px-32 px-0'>
-        <SignUpForm />
+    <div className="w-full py-12">
+      <div className="md:px-16 lg:px-32 px-0">
+        <Suspense fallback={null}>
+          <SignUpForm />
+        </Suspense>
       </div>
-      
     </div>
   );
-};
-
-export default page;
+}
